d5f206ccf545091269eecbf814bab2f6fe535c7d
alvaroG-IA <alvarogarciavelasco1212@gmail.com>
2025-11-15 19:19:16 +0100

    main implemented

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from src.data.dataset import HeartHealthDataset
-from src.models.logistic_bayes import LogisticBayesModel
+import numpy as np
+from data.dataset import HeartHealthDataset
+from models.logistic_bayes import LogisticBayesModel
+from sklearn.metrics import accuracy_score
 
 def main():
+
     data_path = 'data/fallo_cardiaco.csv'
     dataset = HeartHealthDataset(data_path)
+    X, y = dataset.preproces(mode='std')
 
     model = LogisticBayesModel()
+    w = model.fit(X, y)
+
+    pred =  np.where(np.dot(X, w) >= 0.5, 1, 0)
+    acc = accuracy_score(pred, y)
+
+    print(acc)
+
+if __name__ == '__main__':
+    main()
     
