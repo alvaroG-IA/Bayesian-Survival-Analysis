367e8b902cbbd1039dfaca5266a745ccb115c8c5
alvaroG-IA <alvarogarciavelasco1212@gmail.com>
2025-11-16 20:46:05 +0100

    new analyze_posterior() method added

--- src/utils/math_utils.py
+++ src/utils/math_utils.py
@@ -19,3 +19,31 @@ def log_prior(w, mu=0, sigma=10):
     Log-prior gaussiano
     """
     return np.sum(norm.logpdf(w, mu, sigma))
+
+def analyze_posterior(samples, confidence_level=95):
+    """
+    Función que nos ayudará a analizar los valores obtenidos por las muestras, para conocer:
+    - El valor medio
+    - Su desviación estádar, la cual no ayudará a ver como de fiables es ese valor
+    - Intervalos de credibilidad, por defecto se usará un 95% de fiabilidad
+    """
+    
+    # Calcular la media (Valor Central)
+    w_mean = np.mean(samples, axis=0)
+    
+    # Calcular la desviación estándar (Incertidumbre / Fiabilidad)
+    w_std = np.std(samples, axis=0)
+    
+    # Calcular los límites del Intervalo de Credibilidad (CI, ej. 95%)
+    lower_percentile = (100 - confidence_level) / 2
+    upper_percentile = 100 - lower_percentile
+    
+    w_ci_lower = np.percentile(samples, lower_percentile, axis=0)
+    w_ci_upper = np.percentile(samples, upper_percentile, axis=0)
+    
+    return {
+        'mean': w_mean,
+        'std': w_std,
+        'ci_lower': w_ci_lower,
+        'ci_upper': w_ci_upper
+    }
