8c98514baea965f6e2e5dc81805243ce9a3f80cd
alvaroG-IA <alvarogarciavelasco1212@gmail.com>
2025-11-16 20:47:31 +0100

    preditc() method added & changes during training

--- src/models/logistic_bayes.py
+++ src/models/logistic_bayes.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils.math_utils import log_likelihood, log_prior
+from utils.math_utils import log_likelihood, log_prior, analyze_posterior, sigmoid
 from utils.mcmc_utils import metropolis_hastings
 from sklearn.linear_model import LogisticRegression
 
@@ -7,6 +7,10 @@ class LogisticBayesModel:
     def __init__(self, prior_mu=0, prior_sigma=10):
         self.prior_mu = prior_mu
         self.prior_sigma = prior_sigma
+        self.samples_ = None
+        self.w_mean_ = None
+        self.w_std_ = None
+        self.w_ci_ = None
 
     def fit(self, X, y, iterations=6000, burn_in=1000, proposal_width=0.1):
         
@@ -16,7 +20,7 @@ class LogisticBayesModel:
 
         prior_func = lambda w: log_prior(w, self.prior_mu, self.prior_sigma)
 
-        self.samples = metropolis_hastings(
+        self.samples_, self.acceptance_ratio = metropolis_hastings(
             log_likelihood_func=log_likelihood,
             log_prior_func=prior_func,
             initial_w=initial_w,
@@ -27,5 +31,21 @@ class LogisticBayesModel:
             proposal_width=proposal_width
         )
 
-        self.w_mean = np.mean(self.samples, axis=0)
-        return self.w_mean
+        stats = analyze_posterior(samples=self.samples_)
+
+        self.w_mean_ = stats['mean']
+        self.w_std_ = stats['std']
+        self.w_ci_ = (stats['ci_lower'], stats['ci_upper'])
+
+        return self
+    
+    def predict_proba(self, data):
+        if self.w_mean_ is None:
+            raise RuntimeError("Debes de entrenar el modelo .fit() antes de predecir")
+        z = np.dot(data, self.w_mean_)
+        prob = sigmoid(z)
+        return prob
+    
+    def predict(self, data, threshold=0.5):
+        probs = self.predict_proba(data)
+        return (probs >= threshold).astype(int)
