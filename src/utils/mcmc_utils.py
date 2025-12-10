f76b21076954458bab4b766a958d03fb0e2be89a
Álvaro García Velasco <alvarogarciavelasco1212@gmail.com>
2025-12-10 16:29:32 +0100

    Update mcmc_utils.py

--- src/utils/mcmc_utils.py
+++ src/utils/mcmc_utils.py
@@ -4,35 +4,40 @@ import numpy as np
 def metropolis_hastings(log_likelihood_func, log_prior_func, initial_w, X, y,
                         iterations=5000, burn_in=0.05, proposal_width=0.1):
     """
-    Implementación del algoritmo Metropolis-Hasting.
-    :param log_likelihood_func: Función que calcula el logaritmo de la verosimilitud -> log(p(y|X, w)).
-    :param log_prior_func: Función que calcula el logaritmo del prior -> log(p(w)).
-    :param initial_w: Muestra inicial de los pesos para iniciar la cadena MCMC.
-    :param X: Datos de entrada.
-    :param y: Etiquetas de entrada.
-    :param iterations: Número de iteraciones de la cadena MCMC deseadas.
-    :param burn_in: Porcentaje de muestras que serán "quemadas" con el fin de garantizar usar muestras de la
-                    distribución estacionaria
-    :param proposal_width: Valor de la desviación estándar de la distribución de propuesta, el cual controla el
-                           tamaño de los saltos en el espacio de parámetros
+    Implementation of the Metropolis-Hastings algorithm.
+
+    :param log_likelihood_func: Function that computes the log-likelihood -> log(p(y | X, w)).
+    :param log_prior_func: Function that computes the log-prior -> log(p(w)).
+    :param initial_w: Initial weight sample to start the MCMC chain.
+    :param X: Input data.
+    :param y: Input labels.
+    :param iterations: Number of desired MCMC iterations.
+    :param burn_in: Percentage of initial samples to discard to ensure samples come
+                    from the stationary distribution.
+    :param proposal_width: Standard deviation of the proposal distribution, which
+                           controls the jump size in parameter space.
     :return:
-            samples: Muestras obtenidas de la distribución posterior después de aplicar burn-in.
-            acceptance_ratio: Porcentaje de propuestas que son aceptadas.
+            samples: Posterior samples after applying burn-in.
+            acceptance_ratio: Percentage of proposed moves that were accepted.
     """
+
     w = initial_w
     samples = [w]
 
-    # Valor actual del logaritmo de la posterior
+    # Current value of the log-posterior
     log_current = log_likelihood_func(w, X, y) + log_prior_func(w)
     accepted = 0
 
     for _ in range(iterations):
 
+        # Propose a new candidate
         w_new = w + np.random.normal(scale=proposal_width, size=w.shape)
 
+        # Compute log-posterior of the proposed sample
         log_new = log_likelihood_func(w_new, X, y) + log_prior_func(w_new)
         log_accept = log_new - log_current
 
+        # Accept or reject
         if np.log(np.random.rand()) < log_accept:
             w = w_new
             log_current = log_new
@@ -40,8 +45,8 @@ def metropolis_hastings(log_likelihood_func, log_prior_func, initial_w, X, y,
 
         samples.append(w)
 
-    # Eliminación de muestras del burn-in
+    # Burn-in removal
     burn_in_samples = int(burn_in * len(samples))
     samples = samples[burn_in_samples:]
 
-    return np.array(samples), (accepted/iterations) * 100
+    return np.array(samples), (accepted / iterations) * 100
